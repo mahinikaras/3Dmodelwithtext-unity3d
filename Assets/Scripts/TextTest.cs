6e1bf0c9f846bfad8602abac71af17eb8d0c7836
mahinikaras <mahin.ikaras123@gmail.com>
2020-06-21 23:33:49 +0600

    single with text in world space

---
+++ Assets/Scripts/TextTest.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextTest : MonoBehaviour
+{
+	public static TextTest Instance;
+	public Text textComp;
+	public int charIndex;
+	public Canvas canvas;
+	[HideInInspector]public Vector3 worldPos;
+
+	private void Awake()
+	{
+		if (!Instance) Instance = this;
+	}
+	public void PrintPos ()
+	{
+		//charIndex = textComp.text.Length - 1 ;
+		string text = textComp.text;
+		Debug.Log("Text Length : " + text.Length);
+		if (charIndex >= text.Length)
+			return;
+
+		TextGenerator textGen = new TextGenerator (text.Length);
+		Vector2 extents = textComp.gameObject.GetComponent<RectTransform>().rect.size;
+		textGen.Populate (text, textComp.GetGenerationSettings (extents));
+
+		int newLine = text.Substring(0, charIndex).Split('\n').Length - 1;
+		Debug.Log("new Line " + newLine);
+		int whiteSpace = text.Substring(0, charIndex).Split(' ').Length - 1;
+		int indexOfTextQuad = (charIndex * 4) + (newLine * 4) - 4;
+		if (indexOfTextQuad < textGen.vertexCount)
+		{
+			Vector3 avgPos = (textGen.verts[indexOfTextQuad].position + 
+				textGen.verts[indexOfTextQuad + 1].position + 
+				textGen.verts[indexOfTextQuad + 2].position + 
+				textGen.verts[indexOfTextQuad + 3].position) / 4f;
+
+			print (avgPos);
+			PrintWorldPos (avgPos);
+		}
+		else {
+			Debug.LogError ("Out of text bound");
+		}
+	}
+
+	void PrintWorldPos (Vector3 testPoint)
+	{
+		worldPos = textComp.transform.TransformPoint (testPoint);
+		UserInput.Instance.LoadPrefab(worldPos);
+		print (worldPos);
+		new GameObject ("point").transform.position = worldPos;
+		Debug.DrawRay (worldPos, Vector3.up, Color.red, 50f);
+	}
+
+	/*void OnGUI ()
+	{
+		if (GUI.Button (new Rect (10, 10, 100, 80), "Test"))
+		{
+			PrintPos ();
+		}
+	}*/
+}
---
+++ Assets/UserInput.cs
@@ -0,0 +1,91 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum EmojiName
+{
+    happyFace=1,
+    loveEyed=2,
+    glassEyed=3,
+}
+public class UserInput : MonoBehaviour
+{
+    public static UserInput Instance;
+    public Text titleText;
+    public GameObject cube;
+    public int letterCount;
+    public TMP_InputField inputField;
+    public List<GameObject> emojiPrefab;
+    GameObject emoji;
+
+
+    void Start()
+    {
+        if (!Instance) Instance = this;
+    }
+
+  
+
+    public void OnSendButtonPressed()
+    {
+        if (emoji) Destroy(emoji);
+        int index = 0;
+        titleText.text = inputField.text;
+        inputField.text = "";
+        if (titleText.text.Contains("<sprite=5>") || titleText.text.Contains("<sprite=2>") || titleText.text.Contains("<sprite=3>"))
+        {
+            EmojiName emojiName = EmojiName.happyFace;
+            if (titleText.text.Contains("<sprite=5>")) { emoji = emojiPrefab[0];  emojiName = EmojiName.happyFace; }
+            if (titleText.text.Contains("<sprite=2>")) { emoji = emojiPrefab[1]; emojiName = EmojiName.loveEyed; }
+            if (titleText.text.Contains("<sprite=3>")) { emoji = emojiPrefab[2]; emojiName = EmojiName.glassEyed; }
+
+
+            for (int i = 0; i < titleText.text.Length; i++)
+            {
+                if (titleText.text[i] == '<')
+                {
+                    index = i;
+                    break;
+                }
+            }
+            int spcctr = 0;
+            string str1;
+            for (int i = 0; i < index+1; i++)
+            {
+                str1 = titleText.text.Substring(i, 1);
+                if (str1 == " ")
+                    spcctr++;
+            }
+            TextTest.Instance.charIndex = index+1- spcctr;
+            TextTest.Instance.PrintPos();
+
+
+            if (emojiName == EmojiName.happyFace) {  titleText.text = titleText.text.Replace("<sprite=5>", "        ");}
+            if (emojiName == EmojiName.loveEyed) { titleText.text = titleText.text.Replace("<sprite=2>", "        ");}
+            if (emojiName == EmojiName.glassEyed) { titleText.text = titleText.text.Replace("<sprite=3>", "        ");}
+
+        }
+    }
+
+    
+    public void OnEmojiClicked(string emojiText)
+    {
+        
+        Debug.Log("emojitext : " + emojiText);
+        Debug.Log(" inputText.text : " + inputField.text);
+        inputField.text += emojiText;
+    }
+    public void LoadPrefab(Vector3 worldPos)
+    {
+
+         emoji = Instantiate(emoji, new Vector3(worldPos.x+3, worldPos.y, worldPos.z), Quaternion.identity);
+         emoji.transform.localScale = new Vector3(6,6,6);
+         emoji.transform.localRotation = Quaternion.Euler(new Vector3(0,90,0));
+
+    }
+}
